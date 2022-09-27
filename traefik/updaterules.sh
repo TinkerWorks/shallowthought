a6425ec27d4c85a2dcdb9550c50e352b6ef75d33
Alexandru-Sever Horin <alex.sever.h@tinker.works>
2022-09-27 09:51:59 +0300

    Add manual traefik configuration back
    As I run a single node cluster, the traefik should be in forefront as a reverse proxy for all.
    This way, I can filter based on IP

---
+++ traefik/updaterules.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+curl -O https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+curl -O https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+
+sed -e 's/namespace: default/namespace: traefik/' -i kubernetes-crd-rbac.yml
+sed -e 's/namespace: default/namespace: traefik/' -i kubernetes-crd-definition-v1.yml
