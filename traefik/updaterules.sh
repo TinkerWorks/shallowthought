e5bdad3c3d67481fce68f0c09102ed0f02389935
Alexandru-Sever Horin <alex.sever.h@tinker.works>
2024-06-07 11:17:24 +0300

    * bulk changes *

--- traefik/updaterules.sh
+++ traefik/updaterules.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 
-#curl -O https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
-#curl -O https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+curl -O https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+curl -O https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
 
-curl -O https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
-curl -O https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+#curl -O https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+#curl -O https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
 
 sed -e 's/namespace: default/namespace: traefik/' -i kubernetes-crd-rbac.yml
 sed -e 's/namespace: default/namespace: traefik/' -i kubernetes-crd-definition-v1.yml
